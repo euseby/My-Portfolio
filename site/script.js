c705362b8d93f000b2ea53112fcf1ee5e2ce958f
Fodor Eusebiu <euseby779@gmail.com>
2025-01-24 17:05:47 +0200

    Add files via upload

---
+++ site/script.js
@@ -0,0 +1,23 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute("href");
+        document.querySelector(targetId).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
+'use strict';
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    const className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Dark";
+    } else {
+        this.textContent = "Light";
+    }
+});
